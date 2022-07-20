36fa9d33b74ab2b640080f324fb8f0406509d3c9
vinivizzapc <48770688+vinivizzapc@users.noreply.github.com>
2022-07-20 15:44:27 -0300

    ajustando testes de integração

--- src/test/java/com/vvcampos/minhasfinancas/MinhasfinancasApplicationTests.java
+++ src/test/java/com/vvcampos/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinhasfinancasApplicationTests {
+public class MinhasfinancasApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
